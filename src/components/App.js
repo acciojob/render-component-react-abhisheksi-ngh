a1865251e14fdedf0717eaf9bacb223994f231a9
abhishekashj <abhishekkumrsingh9585@gmail.com>
2025-04-01 22:34:34 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+const LearningReact= ()=>{
+  return <p>
+    I am learning React. My life is getting better.
+  </p>
+};
+
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+        <LearningReact/>
+    </div>
   )
 }
 
