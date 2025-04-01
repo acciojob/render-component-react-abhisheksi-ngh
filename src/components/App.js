eddb108eaea7d3ece7fe7050e7b315ce07727252
abhishekashj <abhishekkumrsingh9585@gmail.com>
2025-04-01 22:47:41 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import LearningRect from "./LearningRect";
 
-const LearningReact= ()=>{
-  return <p>
-    I am learning React. My life is getting better.
-  </p>
-};
+// const LearningReact= ()=>{
+//   return <p>
+//     I am learning React. My life is getting better.
+//   </p>
+// };
 
 const App = () => {
   return (
     <div id="main">
-        <LearningReact/>
+        <LearningRect/>
     </div>
   )
 }
---
+++ src/components/LearningRect.js
@@ -0,0 +1,5 @@
+import React from 'react';
+ const LearningRect=()=>{
+    return <p>I am learning React. My life is getting better.</p>;
+ }
+ export default LearningRect;
